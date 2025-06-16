0ed10ca9f2b4763b64af829ef522d3690dae08ed
qhoncheh <ghoncheh.ataee@gmail.com>
2025-06-16 09:29:44 +0330

    changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,10 @@ const  Hero = () => {
             <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
             <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
-        </div>
+      </div>
+      <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white/[0.3] bg-grid-black/[0.2]">
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
     </div>
   )
 }
